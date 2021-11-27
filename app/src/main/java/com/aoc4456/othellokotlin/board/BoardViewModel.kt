416b62fb942d5ae2cc539ce9049c440f4f47ad2a
Aoc4456 <sizu44567789@gmail.com>
2021-11-27 22:32:55 +0900

    次に置ける場所を調べる

--- app/src/main/java/com/aoc4456/othellokotlin/board/BoardViewModel.kt
+++ app/src/main/java/com/aoc4456/othellokotlin/board/BoardViewModel.kt
@@ -5,6 +5,7 @@ import com.aoc4456.othellokotlin.model.Cell
 import com.aoc4456.othellokotlin.model.Stone
 import com.aoc4456.othellokotlin.model.Turn
 import com.aoc4456.othellokotlin.util.PublishLiveData
+import timber.log.Timber
 
 class BoardViewModel() : ViewModel() {
 
@@ -47,6 +48,44 @@ class BoardViewModel() : ViewModel() {
         cellList[upperLeftPosition + 1][upperLeftPosition + 1].stone = Stone.WHITE
         cellList[upperLeftPosition + 1][upperLeftPosition].stone = Stone.BLACK
         cellList[upperLeftPosition][upperLeftPosition + 1].stone = Stone.BLACK
+    }
+
+    /* 引数の場所に置いたとき、ひっくり帰るセルの一覧を取得 **/
+    private fun getCellsToFlip(cell: Cell): List<Cell> {
+        if (cellList[cell.vertical][cell.horizontal].stone != Stone.NONE) {
+            Timber.d("既に石が置いてあります : ${cell.vertical} / ${cell.vertical}")
+            return emptyList()
+        }
+        return getCellsToFlip(cell)
+    }
+
+    /** 置かれた位置から上方向 */
+    private fun getFlipCellUp(placed: Cell): List<Cell> {
+        if (placed.vertical == 0) return emptyList()
+        val upperList = mutableListOf<Cell>()
+        upperList.add(placed)
+        for (i in (placed.vertical - 1) downTo 0) { // 置いた場所のひとつ上から盤の一番上まで
+            upperList.add(cellList[i][placed.horizontal])
+        }
+        return getSandwichedStones(upperList)
+    }
+
+    private fun getSandwichedStones(list:List<Cell>):List<Cell>{
+        // 置いた石
+        val ownStone = list.first()
+        // 置かれている石
+        val otherStones = list.drop(1)
+        // 次の自分の石の位置
+        val endIndex = otherStones.indexOfFirst { it.stone == ownStone.stone }
+        if(endIndex == -1) return emptyList()
+
+        val sandWitched = mutableListOf<Cell>()
+        for(i in 0 until endIndex){
+            // 途中に空白のセルがあったらだめ
+            if(otherStones[i].stone == Stone.NONE) return emptyList()
+            sandWitched.add(otherStones[i])
+        }
+        return sandWitched
     }
 
     companion object {
